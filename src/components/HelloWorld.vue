c9e23a53d4780910f01c7cca87396ab16401b21c
dddw001 <2410656679@qq.com>
2019-02-18 17:47:48 +0800

    初始化项目

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    {{msg}}
+  </div>
+</template>
+
+<script>
+export default {
+  name:'helloWorld',
+  data(){
+   return {
+    msg: 'hi'
+   }
+  }
+}
+</script>
+
+<style>
+
+</style>
